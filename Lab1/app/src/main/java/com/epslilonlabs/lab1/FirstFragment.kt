a35499a858de41d1a6c6a26f65e8e129952ed718
ArianTerra <arianterra@gmail.com>
2023-02-28 19:31:15 +0200

    Lab1 finished

---
+++ Lab1/app/src/main/java/com/epslilonlabs/lab1/FirstFragment.kt
@@ -0,0 +1,39 @@
+package com.epslilonlabs.lab1
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.epslilonlabs.lab1.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.buttonFirst.setOnClickListener {
+            val uri = Uri.parse(getString(R.string.link))
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# android-labs
+
+## How to install
+
+1. Clone this repo
+2. Open any Lab folder in Android studio (not the root repository folder)
+3. Build and start
